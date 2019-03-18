abb470043723b8a5a1a275e669b89de89b2a4117
zhangxianda <zhangxianda@guworks.cc>
2019-03-18 11:56:22 +0800

    build

--- src/components/config/sideConfig.js
+++ src/components/config/sideConfig.js
@@ -203,92 +203,92 @@ const sideText = [
     path: '/siteManage',
     list: []
 },
-{
- title: '安全设置',
- index: '4',
- ico: 'el-icon-view',
- list: [{
-     name: '登陆密码设置',
-     index: '4-1',
-     path: '/passwordChange'
-   },
- ]
-},
-  {
-   title: '活动管理',
-   index: '5',
-   ico: 'el-icon-menu',
-   // path: '/banner',
-   list: [
-     {
-       name: 'banner管理',
-       index: '5-1',
-       path: '/banner'
-     },
-     {
-       name: '活动配置',
-       index: '5-2',
-       path: '/activeConfiguration'
-     }
-   ]
-  },
-  {
-    title: '内容管理',
-    index: '6',
-    ico: 'el-icon-document',
-    // path: '/banner',
-    list: [
-      {
-        name: '教程管理',
-        index: '6-1',
-        path: '/courseManagement'
-      },
-      {
-        name: '案例配置',
-        index: '6-2',
-        path: '/caseManagement'
-      }
-    ]
-  },
-  {
-    title: '系统设置',
-    index: '7',
-    ico: 'el-icon-setting',
-    // path: '/banner',
-    list: [
-      {
-        name: '操作日志',
-        index: '7-1',
-        path: '/operationLog'
-      },
-      {
-        name: '数据备份',
-        index: '7-2',
-        path: '/backup'
-      },
-      {
-        name: '平台基本信息设置',
-        index: '7-3',
-        path: '/informationSet'
-      }
-    ]
-  },
-{
- title: '权限管理',
- index: '8',
- ico: 'el-icon-refresh',
- list: [{
-     name: '员工管理',
-     index: '8-1',
-     path: '/workersManage'
-   },
-   {
-     name: '角色管理',
-     index: '8-2',
-     path: '/roleManage'
-   },
- ]
-},
+// {
+//  title: '安全设置',
+//  index: '4',
+//  ico: 'el-icon-view',
+//  list: [{
+//      name: '登陆密码设置',
+//      index: '4-1',
+//      path: '/passwordChange'
+//    },
+//  ]
+// },
+//   {
+//    title: '活动管理',
+//    index: '5',
+//    ico: 'el-icon-menu',
+//    // path: '/banner',
+//    list: [
+//      {
+//        name: 'banner管理',
+//        index: '5-1',
+//        path: '/banner'
+//      },
+//      {
+//        name: '活动配置',
+//        index: '5-2',
+//        path: '/activeConfiguration'
+//      }
+//    ]
+//   },
+//   {
+//     title: '内容管理',
+//     index: '6',
+//     ico: 'el-icon-document',
+//     // path: '/banner',
+//     list: [
+//       {
+//         name: '教程管理',
+//         index: '6-1',
+//         path: '/courseManagement'
+//       },
+//       {
+//         name: '案例配置',
+//         index: '6-2',
+//         path: '/caseManagement'
+//       }
+//     ]
+//   },
+//   {
+//     title: '系统设置',
+//     index: '7',
+//     ico: 'el-icon-setting',
+//     // path: '/banner',
+//     list: [
+//       {
+//         name: '操作日志',
+//         index: '7-1',
+//         path: '/operationLog'
+//       },
+//       {
+//         name: '数据备份',
+//         index: '7-2',
+//         path: '/backup'
+//       },
+//       {
+//         name: '平台基本信息设置',
+//         index: '7-3',
+//         path: '/informationSet'
+//       }
+//     ]
+//   },
+// {
+//  title: '权限管理',
+//  index: '8',
+//  ico: 'el-icon-refresh',
+//  list: [{
+//      name: '员工管理',
+//      index: '8-1',
+//      path: '/workersManage'
+//    },
+//    {
+//      name: '角色管理',
+//      index: '8-2',
+//      path: '/roleManage'
+//    },
+//  ]
+// },
   {
     title: '素材库',
     index: '9',
